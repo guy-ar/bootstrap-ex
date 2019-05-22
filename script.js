a4db77969cf00686ca3b5480a827ea253c131ac4
guy arbily <guy.arbilly@gmail.com>
2019-05-23 00:31:00 +0100

    final version for jQuery - isue was is overriden link of bootstrap

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 
 $(document).ready(function(){
-    $("#btnTest").click(function(){
-        alert("hi");
-        $(".content").load("main.html"); 
+    // load the main.html when document is ready
+    $(".content").load("main.html"); 
 
+    // go back to main when brand is clicked
+    $("#nav-home").click(function() {
+    
+        // Loading the content of contact.html into to .content class
+        $(".content").load("main.html");    
     });
 
-    $("#btnAbout").click(function() {
-        $.get("about.html", function(data) {
-          $(".content").html(data)
-        })
-      });
-    
-      $("#btnContact").click(function() {
+    // upload contact page
+    $("#nav-contact").click(function() {
+        
         // Loading the content of contact.html into to .content class
         $(".content").load("contact.html");    
-      });
+    });
+    
+    // upload about page
+    $("#nav-about").click(function() {
+        
+        // Loading the content of contact.html into to .content class
+        $(".content").load("about.html");    
+    });
+      
+    
+    
   });
